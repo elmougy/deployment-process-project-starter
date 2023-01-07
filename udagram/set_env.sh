2cacf88091d3a1ccf58d18ab7707dc72fbbbcf0a
Eman Elmougy <e.m.elmougy@gmail.com>
2023-01-07 18:35:13 +0100

    Update set_env.sh

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -2,10 +2,10 @@
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
 export POSTGRES_PASSWORD=myPassword
-export POSTGRES_HOST=mydbinstance.csxbuclmtj3c.us-east-1.rds.amazonaws.com
+export POSTGRES_HOST=postgres.cninqip0t9cr.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::myawsbucket-75139724085
+export AWS_BUCKET=arn:aws:s3:::udacityfrontend123
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=mysecretstring
-export URL=http://localhost:8100
+export URL=http://udagramapidev-env.eba-2mk6vn2i.us-east-1.elasticbeanstalk.com:8100
